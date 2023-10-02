ff771222462ceae4883e6b9e55070f2fdf299052
Gabriel <soutogabriel04@gmail.com>
2023-10-02 17:01:49 -0300

    feat[User]: get user

--- src/recipes-api/Controllers/UserController.cs
+++ src/recipes-api/Controllers/UserController.cs
@@ -12,31 +12,34 @@ namespace recipes_api.Controllers;
 [ApiController]
 [Route("user")]
 public class UserController : ControllerBase
-{    
+{
     public readonly IUserService _service;
-    
+
     public UserController(IUserService service)
     {
-        this._service = service;        
+        this._service = service;
     }
 
     // 6 - Sua aplicação deve ter o endpoint GET /user/:email
     [HttpGet("{email}", Name = "GetUser")]
     public IActionResult Get(string email)
-    {                
-        throw new NotImplementedException();
+    {
+        var user = _service.GetUser(email);
+        if (user == null)
+            return NotFound();
+        return Ok(user);
     }
 
     // 7 - Sua aplicação deve ter o endpoint POST /user
     [HttpPost]
-    public IActionResult Create([FromBody]User user)
+    public IActionResult Create([FromBody] User user)
     {
         throw new NotImplementedException();
     }
 
     // "8 - Sua aplicação deve ter o endpoint PUT /user
     [HttpPut("{email}")]
-    public IActionResult Update(string email, [FromBody]User user)
+    public IActionResult Update(string email, [FromBody] User user)
     {
         throw new NotImplementedException();
     }
@@ -46,5 +49,5 @@ public class UserController : ControllerBase
     public IActionResult Delete(string email)
     {
         throw new NotImplementedException();
-    } 
+    }
 }
